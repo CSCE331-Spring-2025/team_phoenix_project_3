ea15fe7acf754fffbc37fd88566a2a81913db7d9
Miles Ryder <miller1405@tamu.edu>
2025-03-26 11:05:18 -0500

    Node.js setup and database connection

---
+++ db/utils.js
@@ -0,0 +1,17 @@
+import { pool } from "./config";
+
+/**
+ * Send a query to the database.
+ *
+ * @param {*} text
+ * @param {*} params
+ * @returns A list of rows as column name mapped to value or sends an error message to console.
+ */
+export async function query(text, params) {
+	try {
+		const res = await pool.query(text, params);
+		return res.rows;
+	} catch (err) {
+		console.error("Database operation failed:", err.stack);
+	}
+}
