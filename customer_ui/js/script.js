f42bc8c94c58214e2d99495c9870d5c06b61807f
Miles Ryder <miller1405@tamu.edu>
2025-03-28 11:37:51 -0500

    customer_ui should log item map (test)

--- customer_ui/js/script.js
+++ customer_ui/js/script.js
@@ -1,5 +1,10 @@
+import * as db from "../../db/menu-items";
+
+const items = db.getMenuItems();
+
 function order(item) {
     const message = `You ordered: ${item}`;
-    document.getElementById('order-message').textContent = message;
+    document.getElementById("order-message").textContent = message;
     console.log(message);
+    console.log(items[0]);
 }
