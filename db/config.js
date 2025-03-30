23b02150865585e82737108addfacb682fcf96b0
Miles Ryder <miller1405@tamu.edu>
2025-03-29 23:22:29 -0500

    Fixed the back to front connection. customer_ui/js/script.js is able to fetch data and log to consle.

--- db/config.js
+++
@@ -1,9 +0,0 @@
-import pg from "pg";
-
-export const pool = new pg.Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB,
-	password: process.env.DB_PASSWORD,
-	port: 5432,
-});
